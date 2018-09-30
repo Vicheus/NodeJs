2d2ff858c79f9fcf0ced95794ad346c910d05c15
Oleksandr Kononchuk <olexandrkononchuk@gmail.com>
2018-09-30 13:01:17 +0300

    coursera week 1 express and http + morgan logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,8 +7,11 @@ const app      = express();
 const host     = '0.0.0.0';
 const port     = 8090;
 
+app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is Express!</h1></body></html>');
@@ -23,10 +26,3 @@ const server = http.createServer(app);
 server.listen(port, host, () => {
     console.log(`Example app listening on http://${host}:${port}!`);
 });
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(`${__dirname}/public/index.html`));
-// });
-//
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}!`);
-// });
